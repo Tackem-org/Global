0042d7d6a1c491241e6c1701f0bbb711fdf8ab53
Gary The Brown <GaryBrownSunderland@gmail.com>
2022-01-30 23:12:07 +0000

    commit

--- system/webReturns.go
+++ system/webReturns.go
@@ -7,6 +7,14 @@ import (
 	"github.com/Tackem-org/Global/logging/debug"
 )
 
+func QuickWebReturn(statusCode uint32, errorMessage string) (*WebReturn, error) {
+	logging.Debug(debug.FUNCTIONCALLS, "CALLED:[system.ForbiddenWebReturn() (*system.WebReturn, error)]")
+	return &WebReturn{
+		StatusCode:   statusCode,
+		ErrorMessage: errorMessage,
+	}, nil
+}
+
 func ForbiddenWebReturn() (*WebReturn, error) {
 	logging.Debug(debug.FUNCTIONCALLS, "CALLED:[system.ForbiddenWebReturn() (*system.WebReturn, error)]")
 	return &WebReturn{
